889eb56b950fef298aeb31f507fa344c13706e49
alu222 <93721716+alu222@users.noreply.github.com>
2022-10-26 21:35:15 +0800

    Update AVENet1.py

--- AVENet1.py
+++ AVENet1.py
@@ -4,7 +4,6 @@ from torch.nn.parameter import Parameter
 
 from utils.mydata_xu import *
 import torch.nn.functional as F
-## Main NN starts here
 class AVENet1(nn.Module):
 
 	def __init__(self,Bias,f,R,h):
@@ -14,23 +13,22 @@ class AVENet1(nn.Module):
 		# self.h = 8
 		self.w_img = Parameter(torch.randn(R,129,h)) # (4,129,8)
 		self.w_aud = Parameter(torch.randn(R,129,h))
-		# print(w_img)
+		
 		self.w_f  = Parameter(f)
 		self.bias = Parameter(Bias)
-		# print(self.w_f)
-		# print(self.bias)
+		
 
 		self.relu   = F.relu
 		self.imgnet = ImageConvNet()
 		self.audnet = AudioConvNet()
 
-		# Vision subnetwork
+		
 		self.vpool4  = nn.MaxPool2d(14, stride=14)
 		self.vfc1    = nn.Linear(512, 128)
 		self.vfc2    = nn.Linear(128, 128)
 		self.vl2norm = nn.BatchNorm1d(128)
 
-		# Audio subnetwork
+		
 		self.apool4  = nn.MaxPool2d((16, 12), stride=(16, 12))
 		self.afc1    = nn.Linear(512, 128)
 		self.afc2    = nn.Linear(128, 128)
@@ -51,7 +49,7 @@ class AVENet1(nn.Module):
 		img = self.vfc2(img)
 		img = self.vl2norm(img)
 
-		# Audio
+		
 		aud = self.audnet(audio)
 		aud = self.apool4(aud).squeeze(2).squeeze(2)
 		aud = self.relu(self.afc1(aud))
@@ -61,27 +59,27 @@ class AVENet1(nn.Module):
         # join
 		n = img.shape[0] # 获取批次大小
 
-        # 扩充一维
+       
 		img = img.cuda()
 		img = torch.cat([img,torch.ones(n,1).cuda()],dim=1) # (16*129)
 		aud = torch.cat([aud,torch.ones(n,1).cuda()],dim=1)
 
-        # 并行提取各模态特征
+     
 		fusion_img = torch.matmul(img,self.w_img) # (4,16,8)
 		fusion_aud = torch.matmul(aud,self.w_aud)
 
-        # 最终融合
+        
 		fusion_img_aud = fusion_img * fusion_aud # (4,16,8) 对应位置相乘
 		# print(self.w_f.shape) # (1,8)
 		# print(fusion_img_aud.permute(1,0,2).shape) # (16,4,8)
 		fusion_img_aud = torch.matmul(self.w_f,fusion_img_aud.permute(1,0,2)).squeeze() + self.bias
 
-        # 输出结果
+       
 		out = self.fc3(fusion_img_aud)
 		# print(out)
 		# out = self.softmax(out, 1)
 
-        # 测试
+      
 		# print(out)
 		# print(type(out))
 
@@ -116,11 +114,4 @@ if __name__ == '__main__':
 	W_aud = model.w_aud
 	W_f = model.w_f
 	Bias = model.bias
-	print(W_img)
-	print(W_aud)
-	print(W_f)
-	print(Bias)
-	print(o)
-	# o=F.softmax(o,1)
-	# print(o)
-	print(o.shape)#[2,3]
+	
